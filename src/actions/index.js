b6a496a9cc3fadf78f24f879a6aa7b0dd5983cc0
zauzaj <jordanovskibojan@gmail.com>
2016-03-30 10:21:49 +0200

    Deleting post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS= 'FETCH_POSTS';
 export const CREATE_POST= 'CREATE_POST';
 export const FETCH_POST= 'FETCH_POST';
-
+export const DELETE_POST= 'DELETE_POST';
 
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -30,10 +30,19 @@ export function createPost(props) {
 
 
 export function fetchPost(id) {
-  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`,);
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
 
   return {
     type: FETCH_POST,
     payload: request
   };
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return{
+    type: DELETE_POST,
+    payload: request
+  }
 }
